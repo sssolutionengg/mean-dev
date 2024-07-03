28dca99431da449d4fda16076f85e28ad71b8657
Shikher Srivastava <shikher.solutionengineer@gmail.com>
2024-07-03 04:09:52 +0000

    Added blog collection

---
+++ mean-stack-example/01-mongo-db-docker-compose/mongo-init/init-blog.js
@@ -0,0 +1,39 @@
+conn = new Mongo();
+db = conn.getDB("blog");
+db.createCollection("posts");
+db.posts.insertOne({
+    title: "Post Title 1",
+    body: "Body of post.",
+    category: "News",
+    likes: 1,
+    tags: ["news", "events"],
+    date: Date()
+  });
+  db.posts.insertMany([  
+    {
+      title: "Post Title 2",
+      body: "Body of post.",
+      category: "Event",
+      likes: 2,
+      tags: ["news", "events"],
+      date: Date()
+    },
+    {
+      title: "Post Title 3",
+      body: "Body of post.",
+      category: "Technology",
+      likes: 3,
+      tags: ["news", "events"],
+      date: Date()
+    },
+    {
+      title: "Post Title 4",
+      body: "Body of post.",
+      category: "Event",
+      likes: 4,
+      tags: ["news", "events"],
+      date: Date()
+    }
+  ]);
+  
+
